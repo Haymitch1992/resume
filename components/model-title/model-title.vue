22b478a460d087c5aee7415e2ff285821c28cb73
003960 <wangzhiwei3306@163.com>
2021-03-15 17:29:32 +0800

    初始化

---
+++ components/model-title/model-title.vue
@@ -0,0 +1,54 @@
+<template>
+	<view>
+	  <text class="line"></text>
+	  <text class="circle"></text>
+	  <text class="model-Title">
+		{{titleText}}
+	  </text>
+	  <text class="circle"></text>
+	  <text class="line"></text>
+	</view>
+</template>
+
+<script>
+	export default {
+		props:{
+			titleText:String
+		},
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.model-Title{
+	  color: #7e4151;
+	  display: inline-block;
+	  font-size: 32rpx;
+	}
+	.circle{
+	  width: 14rpx;
+	  height: 14rpx;
+	  border: 4rpx solid #9fbfea;
+	  display: inline-block;
+	  border-radius: 50%;
+	  margin: 0 20rpx;
+	}
+	.line{
+	  width: 60rpx;
+	  height: 4rpx;
+	  background-color: #9fbfea;
+	  display: inline-block;
+	  position: relative;
+	  top: -8rpx;
+	}
+</style>
---
+++ pages/skill/skill.vue
@@ -0,0 +1,60 @@
+<template>
+	<view class="container">
+	  <view class="skill-box">
+		  <modelTitle titleText="专业技能"></modelTitle>
+		  <view class="echarts-box">
+		  	<view class="echarts-item">1</view>
+		  	<view>1</view>
+		  	<view>1</view>
+		  	<view>1</view>
+		  </view>
+
+	  </view>
+	  <view class="skill-box">
+		  <modelTitle titleText="专业技能"></modelTitle>
+	  </view>
+	</view>
+</template>
+
+<script>
+	import modelTitle from '../../components/model-title/model-title.vue'
+	export default {
+		components:{
+			modelTitle
+		},
+		data() {
+			return {
+				title: '专业技能'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.skill-box{
+	  background: #fff;
+	  width: 100%;
+	  text-align: center;
+	  margin: 10rpx 0rpx;
+	  border-radius: 16rpx;
+	  padding: 30rpx 0;
+	}
+	.container{
+	  padding: 20rpx;
+	}
+	.echarts-box{
+		display:grid;
+		grid-template-columns: 40vw 40vw;
+		grid-template-rows: 40vw 40vw;
+		justify-content: center;
+		.echarts-item{
+			color: #0000FF;
+		}
+	}
+</style>
