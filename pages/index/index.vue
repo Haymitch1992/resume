866aec875eb204aab972468cd8969b58bf79620c
003960 <wangzhiwei3306@163.com>
2021-03-16 20:06:44 +0800

    页面创建

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,24 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-			<button class="new-page__button-item" @click="navToVue">跳转VUE页面</button>
+		<image class="bg-image" src="/static/bg.png"></image>
+		<view class="my-info"></view>
+		<view class="uni-flex uni-row">
+			<view class="flex-item uni-bg-red" @click="navToVue('../skill/skill')">
+				<image class="tag-image" src="/static/item-5.png"></image>
+				<view>专业技能</view>
+			</view>
+			<view class="flex-item uni-bg-green" @click="navToVue('../experience/experience')">
+				<image class="tag-image" src="/static/item-6.png"></image>
+				<view>项目经验</view>
+			</view>
+			<view class="flex-item uni-bg-blue" @click="navToVue('../work/work')">
+				<image class="tag-image" src="/static/item-7.png"></image>
+				<view>工作经历</view>
+			</view>
+			<view class="flex-item uni-bg-blue" @click="navToVue('../assessment/assessment')">
+				<image class="tag-image" src="/static/item-8.png"></image>
+				<view>自我评价</view>
+			</view>
 		</view>
 	</view>
 </template>
@@ -19,9 +34,9 @@
 
 		},
 		methods: {
-			navToVue(){
-				uni.redirectTo({
-					url: '../assessment/assessment'
+			navToVue(str){
+				uni.navigateTo({
+					url: str
 				});
                 // uni.navigateTo({
                 //     url:'pages/skill/skill'
@@ -31,21 +46,41 @@
 	}
 </script>
 
-<style>
+<style lang="scss">
+	.uni-flex{
+		display: flex;
+		width: 100%;
+	}
+	.my-info{
+		background-color: #fff;
+		padding: 20rpx;
+		width: 100%;
+	}
+	.flex-item {
+		width: 25%;
+		height: 200rpx;
+		text-align: center;
+		line-height: 200rpx;
+		view{
+			height: 60rpx;
+			line-height: 60rpx;
+			font-size: 28rpx;
+		}
+	}
 	.content {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
 	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
+	.bg-image{
+		width: 100%;
+	}
+	.tag-image{
+		width: 100rpx;
+		height: 100rpx;
+		display: block;
+		margin: 0 auto;
 	}
 
 	.text-area {
